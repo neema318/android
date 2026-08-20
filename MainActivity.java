091be2886e514b33383bb08b782809fd0316a612
neema318 <neeema318@gmail.com>
2026-08-20 14:59:23 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,80 +1,69 @@
-package com.example.calculator;
+package com.example.registrationform;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.registrationform.R;
+
 
 public class MainActivity extends AppCompatActivity {
-    private EditText numInput1,numInput2;
-    private TextView resultText;
+
+    EditText name,password;
+
+    RadioGroup gender;
+
+    CheckBox terms;
+
+    Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numInput1 = findViewById(R.id.numInput1);
-        numInput2 = findViewById(R.id.numInput2);
-        resultText = findViewById(R.id.resultText);
-        Button btnAdd = findViewById(R.id.btnAdd);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-        @Override
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        gender = findViewById(R.id.gender);
+        terms = findViewById(R.id.terms);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
+                if (name.getText().toString().isEmpty()){
+                    name.setError("Enter your name");
+                    return;
+            }
 
-                performCalculation ('+');
-            }});
-        Button btnSubtract = findViewById(R.id.btnSubtract);
-        btnSubtract.setOnClickListener(new View.OnClickListener() {
-        @Override
-            public void onClick(View v) {
-                performCalculation ('-');
-            }});
+                if (password.getText().toString().isEmpty()) {
+                    password.setError("Enter password");
+                    return;
+                }
+                if (password.length() < 8) {
+                    password.setError("Password must contain 8 characters");
+                    return;
+                }
 
-        Button btnMultiply = findViewById(R.id.btnMultiply);
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-        @Override
-            public void onClick(View v) {
-                performCalculation ('*');
-            }});
-        Button btnDivide = findViewById(R.id.btnDivide);
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-        @Override
-            public void onClick(View v) {
-                performCalculation ('/');
+                if (gender.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(MainActivity.this, "Select gender", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-            }});
-    }
+                if (!terms.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Accept terms and conditions", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                    Toast.makeText(MainActivity.this,"Registration Successful",Toast.LENGTH_SHORT).show();
+                        }
 
-    private void performCalculation(char operation){
-    String Input1 = numInput1.getText().toString();
-    String Input2 = numInput2.getText().toString();
-    double num1 = Double.parseDouble(Input1);
-    double num2 = Double.parseDouble(Input2);
-    double result = 0;
+                });
 
-    switch (operation){
-        case '+':
-            result = num1 + num2;
-            break;
-
-        case '-':
-            result = num1 - num2;
-            break;
-
-        case '*':
-            result = num1 * num2;
-            break;
-
-        case '/':
-            result = num1/num2;
-            break;
-
-        default:
-            resultText.setText("Invalid operation");
-            return;
-    }
-    resultText.setText("Result: " + result);
-    }
-}
+                }
+        }
